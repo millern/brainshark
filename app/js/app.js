f7a36aaa4535dbea597ec70c4cfee7bebf71a798
Nick Miller <njmiller6@gmail.com>
2013-09-16 21:47:47 -0700

    Changed route provider to state provider

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,13 @@
-var gameApp = angular.module('game', []).
-  config(function($routeProvider, $locationProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/game.html',
-        controller: GameCtrl
-      }).otherwise({redirectTo: 'index.html'});
+var gameApp = angular.module('game', ['ui.router']).
+  config(function($stateProvider, $urlRouterProvider) {
 
-    // configure html5 to get links working
-    // If you don't do this, you URLs will be base.com/#/home rather than base.com/home
-    $locationProvider.html5Mode(true);
+	$urlRouterProvider.otherwise("/game");
+
+    $stateProvider.state('game', {
+		url: '/game',
+		templateUrl: 'partials/game.html',
+		controller: GameCtrl
+	});
+
 });
+
